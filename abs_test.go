11132972631b101231d3241db1ffefb6f13c5295
Daisuke Maki <lestrrat+github@gmail.com>
2018-01-15 12:24:23 +0900

    Make sure to use input/output that can't be optimized away
    https://twitter.com/dgryski/status/952725294187556864

--- abs_test.go
+++ abs_test.go
@@ -1,9 +1,7 @@
 package abs
 
 import (
-	"reflect"
-	"runtime"
-	"strings"
+	"math/rand"
 	"testing"
 )
 
@@ -18,27 +16,37 @@ type absFunc func(int64) int64
 var (
 	testInputs  = []int64{MinInt + 1, MinInt + 2, -1, -0, 1, 2, MaxInt - 1, MaxInt}
 	testOutputs = []int64{MaxInt, MaxInt - 1, 1, 0, 1, 2, MaxInt - 1, MaxInt}
-	testFuncs   = []absFunc{
-		WithBranch,
-		WithStdLib, // test failure expected on large numbers
-		WithTwosComplement,
-		WithASM,
+	testFuncs   = []struct {
+		Name string
+		Func absFunc
+	}{
+		{
+			Name: "WithBranch",
+			Func: WithBranch,
+		},
+		{
+			// test failure expected on large numbers
+			Name: "WithStdLib",
+			Func: WithStdLib,
+		},
+		{
+			Name: "WithTwosComplement",
+			Func: WithTwosComplement,
+		},
+		{
+			Name: "WithASM",
+			Func: WithASM,
+		},
 	}
 )
 
-func funcName(v interface{}) string {
-	s := runtime.FuncForPC(reflect.ValueOf(v).Pointer()).Name()
-	return s[strings.LastIndex(s, ".")+1:]
-}
-
 func TestAbs(t *testing.T) {
-	for _, f := range testFuncs {
-		testName := funcName(f)
-		t.Run(testName, func(t *testing.T) {
+	for _, ts := range testFuncs {
+		t.Run(ts.Name, func(t *testing.T) {
 			for i := 0; i < len(testInputs); i++ {
-				actual := f(testInputs[i])
+				actual := ts.Func(testInputs[i])
 				if actual != testOutputs[i] {
-					t.Errorf("%s(%d)", testName, testInputs[i])
+					t.Errorf("%s(%d)", ts.Name, testInputs[i])
 					t.Errorf("	input:		%064b (%d)", uint64(testInputs[i]), testInputs[i])
 					t.Errorf("	expected:	%064b (%d)", uint64(testOutputs[i]), testOutputs[i])
 					t.Errorf("	actual:		%064b (%d)", uint64(actual), actual)
@@ -48,25 +56,30 @@ func TestAbs(t *testing.T) {
 	}
 }
 
-func BenchmarkWithBranch(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		WithBranch(-1)
-	}
-}
-func BenchmarkWithStdLib(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		WithStdLib(-1)
-	}
-}
+func BenchmarkAbs(b *testing.B) {
+	const maxInputs = 10000
 
-func BenchmarkWithTwosComplement(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		WithTwosComplement(-1)
-	}
-}
+	// Use a set of random inputs so that the compiler cannot optimize it away
+	benchInputs := make([]int64, maxInputs)
 
-func BenchmarkWithASM(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		WithASM(-1)
+	// Store the outputs so that the compiler cannnot optimize them away
+	benchOutputs := make([]int64, maxInputs)
+
+	for i := 0; i < maxInputs; i++ {
+		benchInputs[i] = rand.Int63()
+	}
+
+	for _, ts := range testFuncs {
+		var i int
+		b.Run(ts.Name, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				b.StartTimer()
+				benchOutputs[i] = ts.Func(benchInputs[i])
+				b.StopTimer()
+				if i++; i >= maxInputs {
+					i = 0
+				}
+			}
+		})
 	}
 }
